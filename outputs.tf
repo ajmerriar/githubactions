e58f0b17c79b3a72043942cdab9a3f9a0843d8df
ajmerriar <117775662+ajmerriar@users.noreply.github.com>
2024-05-31 04:25:47 -0400

    Update outputs.tf
    Wrong reference

--- outputs.tf
+++ outputs.tf
@@ -3,31 +3,31 @@ output "resource_group_name" {
 }
 
 output "kubernetes_cluster_name" {
-  value = azurerm_kubernetes_cluster.k8s.name
+  value = azurerm_kubernetes_cluster.aks-cluster.name
 }
 
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_certificate
+  value     = azurerm_kubernetes_cluster.aks-cluster.kube_config[0].client_certificate
   sensitive = true
 }
 
 output "client_key" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_key
+  value     = azurerm_kubernetes_cluster.aks-cluster.kube_config[0].client_key
   sensitive = true
 }
 
 output "cluster_ca_certificate" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate
+  value     = azurerm_kubernetes_cluster.aks-cluster.kube_config[0].cluster_ca_certificate
   sensitive = true
 }
 
 output "cluster_password" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].password
+  value     = azurerm_kubernetes_cluster.aks-cluster.kube_config[0].password
   sensitive = true
 }
 
 output "cluster_username" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].username
+  value     = azurerm_kubernetes_cluster.aks-cluster.kube_config[0].username
   sensitive = true
 }
 
@@ -37,6 +37,6 @@ output "host" {
 }
 
 output "kube_config" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config_raw
+  value     = azurerm_kubernetes_cluster.aks-cluster.kube_config_raw
   sensitive = true
 }
